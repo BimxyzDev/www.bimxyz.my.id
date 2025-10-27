c5e9eba026ac1c69bc05487a202a00809d348c2e
BimxyzDev <bimaputra436123@gmail.com>
2025-10-27 17:37:47 +0700

    Create yanto.js

---
+++ api/yanto.js
@@ -0,0 +1,9 @@
+// api/yanto.js
+// -----------------
+// WARNING: This file exposes your bot token & chat id to anyone who can load the website.
+// Only use this for quick testing. For production, move token to server-side env vars.
+
+window.YANTO = {
+  TOKEN_BOT: "8227663814:AAFtt534u1yvwc2IJtpve1Q-dn6sqE_2sAQ", // contoh: "123456:ABC-DEF..."
+  CHAT_ID: "6629230649"      // contoh: "6629230649" atau "-100xxxxxxxx"
+};
